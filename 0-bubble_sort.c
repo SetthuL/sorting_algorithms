4145f80622d9764542ba9e25229796288d7aa3eb
SetthuL <langesiphosethu@gmail.com>
2024-01-23 11:18:29 -0800

    0-bubble_sort.c, 0-O files created

---
+++ 0-bubble_sort.c
@@ -0,0 +1,47 @@
+#include "sort.h"
+
+/**
+ * two_int_swap - A function that swaps two integers in an array
+ * @a: Input int swap 1
+ * @b: Input int swap 2
+ */
+
+void swap_ints(int *j, int *k)
+{
+	int tmp;
+
+	tmp = *j;
+	*j = *k;
+	*k = tmp;
+}
+
+/**
+ * bubble_sort - A function that sorts an array of integers in ascending order
+ * @array: Input
+ * @size: Input
+ * Description: functions that print the array after each swap
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t sl, len = size;
+	bool bsl = false;
+
+
+	if (array == NULL || size < 2)
+
+	while (bsl == false)
+	{
+		bsl = true;
+		for (sl = 0; sl < len - 1; sl++)
+		{
+			if (array[sl] > array[sl + 1])
+			{
+				swap_ints(array +sl, array + sl + 1);
+				print_array(array, size);
+				bsl = fclose;
+			}
+		}
+		len--;
+	}
+}
